8f9267888e1e949fef1d945351cd1182234d7df7
Jared Still <jkstill@gmail.com>
2025-05-19 13:29:10 -0700

    thumbnail script

---
+++ performance/images/make-thumbnail.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+input_file="$1"
+# make the thumbnail file name match the input file name, but with '_thumb' following the name, but before the extension
+output_file="${input_file%.*}_thumb.${input_file##*.}"
+
+ffmpeg -i $input_file \
+  -vf "scale=150:-1" \
+  -q:v 2 \
+  $output_file
+
+
